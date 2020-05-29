a973cf4c4a7e3b53835726e3605e0fddc4490dcc
Jesper Kristensen <cableman@linuxdev.dk>
2020-05-29 11:03:06 +0200

    DDBTEAM-498: Added generate script

---
+++ generate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+set -eu
+
+java -DapiTests=false -DmodelTests=false -jar /usr/local/opt/swagger-codegen/libexec/swagger-codegen-cli.jar generate -i $1 -l php --config config.json --output "."
+
+rm -rf .swagger-codegen
