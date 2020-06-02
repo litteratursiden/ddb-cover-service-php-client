530e634f16f50e78c94246f12a2f6d3a2e852d65
Jesper Kristensen <cableman@linuxdev.dk>
2020-06-02 13:26:37 +0200

    DDBTEAM-499: Added php-cs-fix to build script

--- generate.sh
+++ generate.sh
@@ -3,5 +3,6 @@
 set -eu
 
 java -DapiTests=false -DmodelTests=false -jar /usr/local/opt/swagger-codegen/libexec/swagger-codegen-cli.jar generate -i $1 -l php --config config.json --output "."
-
 rm -rf .swagger-codegen
+
+composer run apply-coding-standards/php-cs-fixer
